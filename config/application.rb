15a6faf2c77280ec96749f198e9001d70cf03cd6
Ubuntu <ubuntu@ip-172-31-34-135.eu-central-1.compute.internal>
2021-05-29 18:31:17 +0000

    heroku create

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,8 +10,6 @@ module GraduationProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.assets.initialize_on_precompile = false
-
 
     # Configuration for the application, engines, and railties goes here.
     #
